33102b7927e78664357f324476c37bd8bbe4810c
CedricLahaye <cedric.lahaye@my-digital-school.org>
2023-03-01 16:06:56 +0100

    Corrige l'historique

--- calculatrice.js
+++ calculatrice.js
@@ -1,4 +1,6 @@
 let display = document.getElementById('display');
+let historyTable = document.getElementById('history');
+let history = [];
 let currentCalculation = '';
 let currentResult = '';
 
@@ -11,6 +13,9 @@ function calculate() {
     currentResult = eval(currentCalculation);
     display.value = currentResult;
     currentCalculation = '';
+    history.push([currentCalculation, currentResult]);
+	historyTable.innerHTML += '<tr><td>' + currentCalculation + '</td><td>' + currentResult + '</td></tr>';
+	currentCalculation = '';
 }
 
 function clearDisplay() {
@@ -22,15 +27,5 @@ function undo() {
 }
 
 function exportHistory() {
-    let csv = 'Calcul,Résultat\n';
-	for (let i = 0; i < history.length; i++) {
-		csv += history[i][0] + ',' + history[i][1] + '\n';
-	}
-	let csvData = new Blob([csv], {type: 'text/csv;charset=utf-8;'});
-	let csvUrl = URL.createObjectURL(csvData);
-	let hiddenElement = document.createElement('a');
-	hiddenElement.href = csvUrl;
-	hiddenElement.target = '_blank';
-	hiddenElement.download = 'history.csv';
-	hiddenElement.click();
+    alert('Fonctionnalité en cours de développement !');
 }
