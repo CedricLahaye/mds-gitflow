aa05ab1c5fda676d3e0453d0c74a4a4fe7ecbfcd
José <jose@preault.com>
2023-03-05 21:45:29 +0100

    Ajoute la possibilité d'utiliser la pavé numérique pour faire les calculs

--- calculatrice.js
+++ calculatrice.js
@@ -10,18 +10,20 @@ function addToDisplay(value) {
 }
 
 function calculate() {
-    currentResult = eval(currentCalculation);
-    display.value = currentResult;
-    currentCalculation = '';
-    history.push([currentCalculation, currentResult]);
-	historyTable.innerHTML += '<tr id="test"><td>' + currentCalculation + '</td><td>' + currentResult + '</td></tr>';
-	currentCalculation = '';
+	if(currentCalculation !== '') {
+		currentResult = eval(currentCalculation);
+		display.value = currentResult;
+		history.push([currentCalculation, currentResult]);
+		historyTable.innerHTML += '<tr id="test"><td>' + currentCalculation + '</td><td>' + currentResult + '</td></tr>';
+		currentCalculation = '';
+	}
 }
 
 function clearAll() {
     display.value = '';
 	currentCalculation = '';
 	currentResult = '';
+
     history.forEach(() => {
         document.getElementById('test').remove();
     })
@@ -29,7 +31,7 @@ function clearAll() {
 }
 
 function clearCurrent() {
-    display.value = '';
+  display.value = '';
 	currentCalculation = '';
 	currentResult = '';
 }
@@ -47,3 +49,17 @@ function exportHistory() {
 	hiddenElement.download = 'history.csv';
 	hiddenElement.click();
 }
+
+document.addEventListener('keydown', function(event) {
+	event.preventDefault();
+  var keyValue = event.key;
+  var keyCode = event.code;
+	let keyDisplay = ['Numpad0','Numpad1','Numpad2','Numpad3','Numpad4','Numpad5','Numpad6','Numpad7','Numpad8','Numpad9','NumpadMultiply','NumpadDivide','NumpadSubtract','NumpadAdd','NumpadDecimal'];
+
+	if(keyDisplay.includes(keyCode)){
+    currentCalculation += keyValue;
+    display.value = currentCalculation;
+	} else if (keyCode === 'NumpadEnter'){
+		calculate();
+	}
+});
